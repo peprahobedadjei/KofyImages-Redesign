fb149637d0399b5de4acfbf048c2227e6225869e
peprahobedadjei <nanakwameadjeipeprah@gmail.com>
2025-07-05 23:44:48 +0100

    adjusted appbar height

--- lib/constants/custom_appbar.dart
+++ lib/constants/custom_appbar.dart
@@ -122,5 +122,5 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
   }
 
   @override
-  Size get preferredSize => Size.fromHeight(80.h);
+  Size get preferredSize => Size.fromHeight(120.h);
 }
